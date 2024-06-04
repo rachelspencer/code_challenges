85ce06d78edd1157e4bffbc781ba236eae553730
Rachel Spencer <rachelspencer688@gmail.com>
2024-06-04 13:09:09 -0400

    install jest and write tests for code challenge solution

---
+++ find_the_odd_int/find_the_odd_int.test.js
@@ -0,0 +1,12 @@
+const findOddInt = require('./find_the_odd_int.js');
+
+describe('findOddInt', () => {
+    test('should return the integer that appears an odd number of times', () => {
+        expect(findOddInt([7])).toBe(7);
+        expect(findOddInt([0])).toBe(0);
+        expect(findOddInt([1, 1, 2])).toBe(2);
+        expect(findOddInt([0, 1, 0, 1, 0])).toBe(0);
+        expect(findOddInt([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1])).toBe(4);
+        // Add more test cases as needed
+    });
+});
