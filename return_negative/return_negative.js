dbbe74aabc44823ef6d57f033a077893295dd1a8
Rachel Spencer <rachelspencer688@gmail.com>
2024-06-06 12:15:16 -0400

    second refactor for return_negative code challenge

--- return_negative/return_negative.js
+++ return_negative/return_negative.js
@@ -1,10 +1,12 @@
-function makeNegative(num) {
-    if (num <= 0){
-        return num
-    } else 
-    return num * -1
-  };
+// function makeNegative(num) {
+//     if (num <= 0){
+//         return num
+//     } else 
+//     return num * -1
+//   };
 
-console.log(makeNegative(0));
+function makeNegative(num) {
+    return num < 0 ? num : num * -1
+}
 
 module.exports = makeNegative
