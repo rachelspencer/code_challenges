36bcb509c6409e6702f7c6e740d26f88a1675fde
Rachel Spencer <rachelspencer688@gmail.com>
2024-06-19 11:23:00 -0400

    first solution for merge two sorted lists

---
+++ merge_two_sorted_lists/mergeTwoSortedLists.js
@@ -0,0 +1,45 @@
+// You are given the heads of two sorted linked lists `list1` and `list2`.
+
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+
+// Example 1:
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+
+// Example 2: 
+// Input: list1 = [], list2 = []
+// Output: []
+
+// Example 3:
+// Input: list1 = [], list2 = [0]
+// Output: [0]
+
+const list1 = [1,3,6,4,8]
+const list2 = [5,6,2]
+
+
+const mergeTwoLists = function(list1, list2) {
+
+    if (list1.length < 1 && list2.length < 1) {
+        return [];
+
+    } else if  (list1.length < 1) {
+        return [...list2].sort();
+
+    } else if (list2.length < 1) {
+        return [...list1].sort();
+
+    } else {
+        const mergedArr = [...list2]
+        mergedArr.push(...list1)
+       
+        return mergedArr.sort();
+    }
+};
+
+console.log(mergeTwoLists(list1, list2))
+
+module.exports = mergeTwoLists
