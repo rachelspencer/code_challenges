7e0d62c74864432361c9efff67fe545e94fc71a4
Rachel Spencer <rachelspencer688@gmail.com>
2024-06-20 10:43:14 -0400

    refactor solution for merge two lists

--- merge_two_sorted_lists/mergeTwoSortedLists.js
+++ merge_two_sorted_lists/mergeTwoSortedLists.js
@@ -20,26 +20,45 @@
 const list1 = [1,3,6,4,8]
 const list2 = [5,6,2]
 
+// Solution 1
+// const mergeTwoLists = function(list1, list2) {
+
+//     if (list1.length < 1 && list2.length < 1) {
+//         return [];
+
+//     } else if  (list1.length < 1) {
+//         return [...list2].sort((a, b) => a - b);;
+
+//     } else if (list2.length < 1) {
+//         return [...list1].sort((a, b) => a - b);;
+
+//     } else {
+//         const mergedArr = [...list2]
+//         mergedArr.push(...list1)
+       
+//         return mergedArr.sort((a, b) => a - b);;
+//     }
+// };
+
+
+// Refactor 
 
 const mergeTwoLists = function(list1, list2) {
 
-    if (list1.length < 1 && list2.length < 1) {
+    if (!list1.length && !list2.length) {
         return [];
 
-    } else if  (list1.length < 1) {
+    } else if  (!list1.length) {
         return [...list2].sort((a, b) => a - b);;
 
-    } else if (list2.length < 1) {
+    } else if (!list2.length) {
         return [...list1].sort((a, b) => a - b);;
 
     } else {
-        const mergedArr = [...list2]
-        mergedArr.push(...list1)
-       
-        return mergedArr.sort((a, b) => a - b);;
+        const mergedArr = [...list1, ...list2]
+        return mergedArr.sort((a,b) => a - b);
+
     }
 };
-
-// console.log(mergeTwoLists(list1, list2))
 
 module.exports = mergeTwoLists
