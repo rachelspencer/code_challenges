59e4c8c31a0d959d2e670e9c037b480ff9f708c6
Rachel Spencer <rachelspencer688@gmail.com>
2024-09-06 13:11:07 -0400

    rename variables

--- make_phone_number/make_phone_number.js
+++ make_phone_number/make_phone_number.js
@@ -1,17 +1,16 @@
-const phoneNumber = ([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+const numbers = ([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
 // Solution 1
 // function createPhoneNumber(numbers){
-//   phoneNumber.splice(0, 0, '(')
-//   phoneNumber.splice(4, 0, ') ')
-//   phoneNumber.splice(8, 0, '-')
-//     return phoneNumber.join('');
+//   numbers.splice(0, 0, '(')
+//   numbers.splice(4, 0, ') ')
+//   numbers.splice(8, 0, '-')
+//     return numbers.join('');
   
 // }
 
 // Solution 2 
 function createPhoneNumber(numbers){
- return `(${phoneNumber.slice(0,3).join('')}) ${phoneNumber.slice(3, 6).join('')}-${phoneNumber.slice(6).join('')}`
+ return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6).join('')}`
 
 }
-console.log(createPhoneNumber(phoneNumber));
-
+console.log(createPhoneNumber(numbers));
