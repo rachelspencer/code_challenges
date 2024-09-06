7dd75477d2a55e42f75de7a5a5a62d1a1bb7f9e6
Rachel Spencer <rachelspencer688@gmail.com>
2024-09-06 13:05:50 -0400

    solution 2

--- make_phone_number/make_phone_number.js
+++ make_phone_number/make_phone_number.js
@@ -1,12 +1,17 @@
 const phoneNumber = ([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
-
-function createPhoneNumber(numbers){
-  phoneNumber.splice(0, 0, '(')
-  phoneNumber.splice(4, 0, ') ')
-  phoneNumber.splice(8, 0, '-')
-  return phoneNumber.join('');
+// Solution 1
+// function createPhoneNumber(numbers){
+//   phoneNumber.splice(0, 0, '(')
+//   phoneNumber.splice(4, 0, ') ')
+//   phoneNumber.splice(8, 0, '-')
+//     return phoneNumber.join('');
   
-}
+// }
 
+// Solution 2 
+function createPhoneNumber(numbers){
+ return `(${phoneNumber.slice(0,3).join('')}) ${phoneNumber.slice(3, 6).join('')}-${phoneNumber.slice(6).join('')}`
+
+}
 console.log(createPhoneNumber(phoneNumber));
 
