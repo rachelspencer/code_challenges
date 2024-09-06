623c3c583a2ae01e8ba8ce57657bb8f2d05ce788
Rachel Spencer <rachelspencer688@gmail.com>
2024-09-06 13:21:46 -0400

    correct file name for jest test

---
+++ make_phone_number/make_phone_number_prefix.test.js
@@ -0,0 +1,7 @@
+const createPhoneNumber = require('./make_phone_number');
+
+describe('createPhoneNumber', () => {
+    test('should return numbers formatted into a phone number', () => {
+        expect(createPhoneNumber(["1,2,3,4,5,6,7,8,9,0"])).toBe("(123) 456-7890");
+    });
+});
